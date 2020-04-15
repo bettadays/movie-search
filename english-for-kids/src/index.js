46b5369e16b78d1c83ea705c1fe9c0b96f257097
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-04-15 10:34:07 +0300

    feat: add toggle

--- english-for-kids/src/index.js
+++ english-for-kids/src/index.js
@@ -1,1 +1,14 @@
-import {a} from './js/test.js';
+const mode = document.querySelector('.mode');
+const modeName = document.querySelector('.mode__name');
+const toggle = document.querySelector('.mode__toggle');
+
+mode.addEventListener('click', () => {
+  if(mode.classList.contains('mode_toggled')){
+    modeName.innerHTML = 'TRAIN';
+  } else modeName.innerHTML = 'PLAY';
+   mode.classList.toggle('mode_toggled');
+   modeName.classList.toggle('mode__name_toggled');
+   toggle.classList.toggle('mode__toggle_toggled');
+
+
+})
