f138d3d2dcffd095b4a3fb08641f7286cbe2ec67
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-04-17 15:30:36 +0300

    feat: add category cards list rendering from data

---
+++ english-for-kids/src/js/WordsList.js
@@ -0,0 +1,19 @@
+import {WordCard} from './WordCard.js';
+
+export class WordsList {
+  constructor(data, category, mode) {
+    this.category = category;
+    this.data = data;
+    this.mode = mode;
+  }
+  generate() {
+    this.data.forEach(item => {
+      if (item.catName === this.category) {
+        item.data.forEach(item => {
+          const wordCard = new WordCard(item, this.mode);
+          wordCard.generate();
+        })
+      }
+    })
+  }
+}
