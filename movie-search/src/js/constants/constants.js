355f34dcd7a2c1c6a61fc15c653f32e0a6b7c542
Tetiana Ovchar <tetianaovchar@gmail.com>
2020-05-18 19:27:33 +0300

    refactor: place constants in separate file

---
+++ movie-search/src/js/constants/constants.js
@@ -0,0 +1,23 @@
+export const INIT_MOVIE_REQUEST = 'dream';
+
+export const API_KEY_YANDEX = 'trnsl.1.1.20200507T084733Z.d29d276f95af1481.d6d61214e6ae761cf99f7225b5b4823448d5ddd8';
+
+export const BASE_URL_YANDEX = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=';
+export const API_KEY_OMDB = '2f52bee9';
+export const BASE_URL_OMDB = 'https://www.omdbapi.com/?apikey=';
+
+export const BASE_URL_POSTER = 'https://www.imdb.com/title/';
+
+export const ERROR_MSG_COLOR = 'red';
+export const INFO_MSG_COLOR = 'blue';
+
+export const RESULTS_FOR = 'Showing results for';
+export const MOVIES_FOUND = 'movies were found';
+export const NO_RESULTS = 'No results found for';
+export const REQUEST_LIMIT = 'Request limit reached';
+
+
+
+
+
+
